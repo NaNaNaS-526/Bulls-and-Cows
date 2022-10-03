18e33eed60385f3809835ab31279b4c776ce4586
NaNaNaS <tairvildanov7@gmail.com>
2022-10-03 20:35:45 +0500

    Added a function to convert a number to an array

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,18 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static System.Char;
+using Random = UnityEngine.Random;
 
 public class GameScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI numberOfMatchesText;
+    [SerializeField] private TMP_InputField playerInputField;
+    [SerializeField] private Button enterButton;
+
+    private int _numberLength = 4;
+    private int _hiddenNumber;
+    private string _inputText;
+
+    private byte[] _hiddenNumberArray;
+    private byte[] _inputNumberArray;
+
+    private void Start()
     {
-        
+        enterButton.onClick.AddListener(SearchForMatches);
+        _hiddenNumber = Random.Range(1000, 10000);
+        _hiddenNumberArray = ConvertIntegerToList(_hiddenNumber);
+        _inputText = playerInputField.text;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    private void SearchForMatches()
+    {
+        if (_inputText.Length == _numberLength)
+        {
+        }
+    }
+
+    private byte[] ConvertIntegerToList(int number)
+    {
+        var listOfNumbers = new List<byte>();
+        foreach (var a in number.ToString().ToCharArray())
+        {
+            listOfNumbers.Add((byte)GetNumericValue(a));
+        }
+
+        return listOfNumbers.ToArray();
     }
 }
